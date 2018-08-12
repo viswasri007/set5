5abb736537fa11e5e13487ae895031cdec588f96
viswasri007 <41729456+viswasri007@users.noreply.github.com>
2018-08-12 11:41:22 +0530

    Create 46.java

---
+++ 46.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Guvi
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		n=n+1;
+		System.out.println(n);
+	}
+}
