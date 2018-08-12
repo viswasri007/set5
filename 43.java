0cb9f0fca99358996f6658dfbebfad4e759e8476
viswasri007 <41729456+viswasri007@users.noreply.github.com>
2018-08-12 10:42:42 +0530

    Create 43.java

---
+++ 43.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Guvi
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		String firstStr = sc.next();
+		String secondStr = sc.next();
+		System.out.println(firstStr+secondStr);
+	}
+}
