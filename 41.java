89d99a0056b6ef675cadff4ee08c129f8715872a
viswasri007 <41729456+viswasri007@users.noreply.github.com>
2018-08-12 10:32:47 +0530

    Create 41.java

---
+++ 41.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Guvi
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+			Scanner sc=new Scanner(System.in);
+		String str=sc.nextLine();
+		int n=sc.nextInt();
+		int i=1;
+		while(i<=n)
+		{
+			System.out.println(str);
+			i++;
+		}
+	}
+}
