0dc478bb55ff0f773b335f00142e59b2235cc825
viswasri007 <41729456+viswasri007@users.noreply.github.com>
2018-08-12 12:14:28 +0530

    Create 47.java

---
+++ 47.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Guvi
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int a[]=new int[n];
+		for(int i=0;i<n;i++)
+		{
+			a[i]=sc.nextInt();
+		}
+		int max=a[0];
+		int min=a[0];
+		for(int i=0;i<n;i++)
+		{
+			if(a[i]>max)
+			{
+				max=a[i];
+			}
+			if(a[i]<min)
+			{
+				min=a[i];
+			}
+		}
+		System.out.println(min+" "+max);
+	}
+}
